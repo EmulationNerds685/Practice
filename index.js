1252dabc2847569d785dc9039922eac7ba39a27f
EmulationNerds685 <bhaskartiwari270@gmail.com>
2024-05-11 21:39:09 +0530

    New code added

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-let posts = [
-	{
-		id: 1,
-		title: 'The Rise of Decentralized Finance',
-		content:
-			'Things have been changed. From Now no one is in boundation to no one',
-		author: 'Alex Thompson',
-		date: '2023-08-01T10:00:00Z'
-	},
-	{
-		id: 2,
-		title: 'The Impact of Artificial Intelligence on Modern Businesses',
-		content:
-			"Artificial Intelligence (AI) is no longer a concept of the future. It's very much a part of our present, reshaping industries and enhancing the capabilities of existing systems. From automating routine tasks to offering intelligent insights, AI is proving to be a boon for businesses. With advancements in machine learning and deep learning, businesses can now address previously insurmountable problems and tap into new opportunities.",
-		author: 'Mia Williams',
-		date: '2023-08-05T14:30:00Z'
-	}
-]
-console.log(posts)
-console.log('Hello')
+var a = 47
+var b = 88
+console.log(a + b)
