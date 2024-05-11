3f366ba02b4205cee72b80a449e8a649e2038dd4
EmulationNerds685 <bhaskartiwari270@gmail.com>
2024-05-11 21:14:51 +0530

    Post 3 removed

--- index.js
+++ index.js
@@ -14,14 +14,6 @@ let posts = [
 			"Artificial Intelligence (AI) is no longer a concept of the future. It's very much a part of our present, reshaping industries and enhancing the capabilities of existing systems. From automating routine tasks to offering intelligent insights, AI is proving to be a boon for businesses. With advancements in machine learning and deep learning, businesses can now address previously insurmountable problems and tap into new opportunities.",
 		author: 'Mia Williams',
 		date: '2023-08-05T14:30:00Z'
-	},
-	{
-		id: 3,
-		title: 'Sustainable Living: Tips for an Eco-Friendly Lifestyle',
-		content:
-			"Sustainability is more than just a buzzword; it's a way of life. As the effects of climate change become more pronounced, there's a growing realization about the need to live sustainably. From reducing waste and conserving energy to supporting eco-friendly products, there are numerous ways we can make our daily lives more environmentally friendly. This post will explore practical tips and habits that can make a significant difference.",
-		author: 'Samuel Green',
-		date: '2023-08-10T09:15:00Z'
 	}
 ]
 console.log(posts)
