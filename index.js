a27e704dcb60ed9b1c1e369983b9175410ec1258
EmulationNerds685 <bhaskartiwari270@gmail.com>
2024-05-11 21:53:53 +0530

    Feature added

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 var a = 47
 var b = 88
-console.log(a + b)
+var c = 22
+var d = a + b + c
+var e = a + b + c / d
+var f = a + b + (c / d) * e
+console.log(d)
+console.log(e)
+console.log(f)
+console.log('Happy Coding')
+console.log('Things are Awesome')
