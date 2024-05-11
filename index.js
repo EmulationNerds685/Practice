02beeb40c749c51ab7a63f94d7c17973e4ff1180
EmulationNerds685 <bhaskartiwari270@gmail.com>
2024-05-11 21:29:57 +0530

    Paragraph modeified

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let posts = [
 		id: 1,
 		title: 'The Rise of Decentralized Finance',
 		content:
-			'Decentralized Finance (DeFi) is an emerging and rapidly evolving field in the blockchain industry. It refers to the shift from traditional, centralized financial systems to peer-to-peer finance enabled by decentralized technologies built on Ethereum and other blockchains. With the promise of reduced dependency on the traditional banking sector, DeFi platforms offer a wide range of services, from lending and borrowing to insurance and trading.',
+			'Things have been changed. From Now no one is in boundation to no one',
 		author: 'Alex Thompson',
 		date: '2023-08-01T10:00:00Z'
 	},
